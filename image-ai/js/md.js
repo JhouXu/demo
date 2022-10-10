c4cd24f519b20cd11d7008ec87c99e2c954f4987
JhouXu <2324837581@qq.com>
2022-10-10 18:15:21 +0800

    Fix: rtc摄像头逻辑封装，屏幕截图

--- image-ai/js/md.js
+++ image-ai/js/md.js
@@ -1,17 +1,40 @@
 "use strict";
 
-// 浏览器兼容判断
-if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-  throw new Error("enumerateDevices is not supported");
+document.querySelector("#btn").addEventListener("click", () => captureHandle("#rtc"));
+function captureHandle(captureId) {
+  const dom = document.querySelector(captureId);
+  const { devicePixelRatio } = window;
+  const { offsetWidth, offsetHeight } = dom;
+
+  html2canvas(dom, {
+    useCORS: true,
+    scale: devicePixelRatio,
+    width: offsetWidth,
+    height: offsetHeight,
+  })
+    .then((canvas) => {
+      console.log(canvas.toDataURL("image/png"));
+    })
+    .catch((err) => {
+      console.log(`html2canvas 内部执行发生错误： ${err}`);
+    });
 }
 
-const constraints = { audio: false, video: { facingMode: { exact: "environment" } } };
-navigator.mediaDevices
-  .getUserMedia(constraints)
-  .then((stream) => {
-    let video = document.querySelector("#rtc");
-    video.srcObject = stream;
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+initCamera("#rtc");
+function initCamera(renderId) {
+  // 浏览器兼容判断
+  if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+    throw new Error("enumerateDevices is not supported");
+  }
+
+  const constraints = { audio: false, video: { facingMode: { exact: "environment" } } };
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((stream) => {
+      let video = document.querySelector(renderId);
+      video.srcObject = stream;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
