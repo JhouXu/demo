edc2f50ac57964e0dfebfffa2c29e3ba27d2558c
JhouXu <2324837581@qq.com>
2022-12-02 18:33:47 +0800

    Fix: 添加模型事件监听，优化代码结构，使用绝对地址

--- js-ar/js/nft.js
+++ js-ar/js/nft.js
@@ -1,10 +1,11 @@
-let isPlay = false;
+window.isPlay = false;
 
 // aframe 绑定处理程序
 AFRAME.registerComponent("markerhandler", {
   init: function () {
-    let { sceneEl } = this.el;
-    let dom = document.querySelector(".e-entity");
+    let { sceneEl } = this.el; // this.el === <a-nft></a-nft>>
+    let dom = document.querySelector(".e-entity"); // dom === <a-entity></a-entity>
+
     // 图像出现在视野时，播放模型骨骼动画
     sceneEl.addEventListener("markerFound", () => {
       setTimeout(() => {
@@ -27,15 +28,7 @@ window.onload = function () {
     alert("open camera");
   });
 
-  document.querySelector("#play-pause-handler").addEventListener("click", (e) => {
-    if (isPlay) {
-      isPlay = false;
-      document.querySelector(".e-entity").setAttribute("animation-mixer", { timeScale: 0 });
-    } else {
-      document.querySelector(".e-entity").setAttribute("animation-mixer", { timeScale: 1, loop: "repeat" });
-      isPlay = true;
-    }
-  });
+  document.querySelector("#play-pause-handler").addEventListener("click", (e) => animController(".e-entity", "isPlay"));
 
   document.querySelector("#get-position").addEventListener("click", (e) => {
     getPosition()
@@ -46,4 +39,18 @@ window.onload = function () {
         console.log(err);
       });
   });
+
+  // 尝试侦听模型渲染位置的事件
+  const EntityDOM = document.querySelector(".e-entity");
+  EntityDOM.addEventListener("click", (e) => animController(".e-entity", "isPlay"));
 };
+
+function animController(domStr, storeName) {
+  if (window[storeName]) {
+    window[storeName] = false;
+    document.querySelector(domStr).setAttribute("animation-mixer", { timeScale: 0 });
+  } else {
+    window[storeName] = true;
+    document.querySelector(domStr).setAttribute("animation-mixer", { timeScale: 1, loop: "repeat" });
+  }
+}
